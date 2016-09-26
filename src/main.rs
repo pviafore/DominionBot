1454a7c0ae390fa0ed7ad56213160aaca0575273
Pat Viafore <patviafore@gmail.com>
2016-09-25 20:25:49 -0500

    Getting JSON Parsing done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+mod parser;
+
 fn main() {
-    println!("Hello, world!");
+    run_game_loop();
 }
+
+
+fn run_game_loop() {
+    loop {
+        let input = get_input();
+        let output = parser::parse_message(input);
+        if !output.is_empty(){
+            println!("{}", output);
+        }
+        else {
+            break;
+        }
+    } 
+}
+
+fn get_input() -> String{
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Did not get proper input");
+    String::from(input.trim())
+}
+
+fn process_input(_input: String) -> String {
+    String::from("String")
+}
+
